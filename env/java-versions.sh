79de69ff519b014ce1c20ed845333318cd327336
john guthrie <grafpoo@gmail.com>
2019-12-11 13:47:12 -0500

    fixed home dir

--- env/java-versions.sh
+++ env/java-versions.sh
@@ -1,3 +1,3 @@
-[ -s "/Users/jponge/.jabba/jabba.sh" ] && \
-source "/Users/jponge/.jabba/jabba.sh" && \
+[ -s "/Users/jguthrie/.jabba/jabba.sh" ] && \
+source "/Users/jguthrie/.jabba/jabba.sh" && \
 jabba use zulu@1.11.0-4
